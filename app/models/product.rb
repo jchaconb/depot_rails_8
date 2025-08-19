129f641469134fc858bd49059cf7dedf5d5a2cad
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-08-19 14:39:16 -0600

    Improve product index UI and enable real-time updates via Hotwire

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   has_one_attached :image
+
+  # This tells Rails to broadcast changes to the product model to any clients that are listening.
+  after_commit -> { broadcast_refresh_later_to "products" }
 end

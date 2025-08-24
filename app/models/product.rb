1282a8034ff93f4282bf1cc8c9fbc2caf6cd854e
Jose Chacon <josealberto.ch.b@gmail.com>
2025-08-24 07:21:29 -0600

    Add validations to the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,25 @@
 class Product < ApplicationRecord
   has_one_attached :image
 
-  # This tells Rails to broadcast changes to the product model to any clients that are listening.
+  # This tells Rails to broadcast changes to the product
+  # model to any clients that are listening.
   after_commit -> { broadcast_refresh_later_to "products" }
+
+  validates :title, :description, :image, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  validate :acceptable_image
+
+  ACCEPTABLE_TYPES = [ "image/gif", "image/jpeg", "image/png" ]
+
+  private
+
+  def acceptable_image
+    return unless image.attached?
+
+    unless ACCEPTABLE_TYPES.include?(image.content_type)
+      errors.add(:image, "must be a GIF, JPEG or PNG")
+    end
+  end
 end
